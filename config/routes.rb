20cfad7d52e9ec419128a854d1e4fd6184faf5f9
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2019-09-29 19:01:28 -0300

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, only: %i[registrations sessions]
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index'
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new'
+    end
+  end
 end
