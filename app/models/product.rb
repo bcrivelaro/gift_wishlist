ceeda8ad4ee8ea8dd10bd27c59d74d3d5f58f327
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2019-09-29 19:50:19 -0300

    remove activestorage tables

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_one_attached :image
 
   validates :name, presence: true, uniqueness: true
   validates :price, numericality: { greater_than: 0 }
